c451ec59f870e96882cbe4d8d24e5c45aabb28ae
matsumoto yusuke <yuyu580905@yahoo.co.jp>
2026-08-05 12:04:02 +0900

    バリデーションルール名前の文字数制限を20文字から255文字に修正

--- src/app/Actions/Fortify/CreateNewUser.php
+++ src/app/Actions/Fortify/CreateNewUser.php
@@ -21,7 +21,7 @@ class CreateNewUser implements CreatesNewUsers
     {
         //新規登録時にRegisterRequestは用いられないため、こちらでバリデーションを設定する必要あり
         $rules = [
-            'name' => ['required', 'string', 'max:20'],
+            'name' => ['required', 'string', 'max:255'],
             'email' => [
                 'required',
                 'string',
@@ -34,7 +34,7 @@ class CreateNewUser implements CreatesNewUsers
         $messages = [
             'name.required' => 'お名前を入力してください',
             'name.string' => 'お名前は文字で入力してください',
-            'name.max' => 'お名前は20字以下で入力してください',
+            'name.max' => 'お名前は255字以下で入力してください',
             'email.required' => 'メールアドレスを入力してください',
             'email.string' => 'メールアドレスは文字で入力してください',
             'email.email' => 'メールアドレスはメール形式で入力してください',
